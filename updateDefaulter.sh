f5e1e628fd5f6dd362834faa3dfda7141531539d
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-05-18 19:22:18 +0530

    Create updateDefaulter.sh

---
+++ updateDefaulter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for room in $HOME/*; do
+  if [ -d $room ]; then
+    for student in $room/*; do
+      if [ -d $student ]; then
+        if [ $(awk 'NR==2 {print $2}' $student/fees.txt) -eq 0 ];then
+        rollnumber=$(awk -F ': ' '/Roll Number:/ {print $2}' $student/userDetails.txt)
+        echo $(basename $student) $rollnumber >> $HOME/feeDefaulters.txt
+        fi
+      fi
+    done
+  fi
+done
