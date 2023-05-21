bd7c9d574b3cdbe48db711e6ff9b71c933254a07
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-05-21 11:07:35 +0530

    Update feeBreakup.sh

--- feeBreakup.sh
+++ feeBreakup.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-awk '/TuitionFee/ {$2=$2+50000; print}' $HOME/fees.txt > temp.txt && mv temp.txt $HOME/fees.txt
-awk '/HostelRent/ {$2=$2+20000; print}' $HOME/fees.txt > temp.txt && mv temp.txt $HOME/fees.txt
-awk '/ServiceCharge/ {$2=$2+10000; print}' $HOME/fees.txt > temp.txt && mv temp.txt $HOME/fees.txt
-awk '/MessFee/ {$2=$2+20000; print}' $HOME/fees.txt > temp.txt && mv temp.txt $HOME/fees.txt
+awk '/TuitionFee/ {$2=$2+50000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
+awk '/HostelRent/ {$2=$2+20000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
+awk '/ServiceCharge/ {$2=$2+10000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
+awk '/MessFee/ {$2=$2+20000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
 
 echo "Date of fee submission :- $date" >> $HOME/fees.txt
-hostel=$(direname $(dirname $HOME))
+hostel=$(dirname $(dirname $HOME))
 if [ $(cat $hostel/announcements.txt | wc -l) -lt 5 ]
 then
     echo "$(awk -F ': ' '/Roll Number:/ {print $2}' $HOME/userDetails.txt)" >> $hostel/announcements.txt
 fi
 echo "Fee successfully submitted"
-
