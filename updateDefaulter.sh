a90ea6294b3d8659f17c6b61399412b83a90a62d
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-05-21 12:21:55 +0530

    Update updateDefaulter.sh

--- updateDefaulter.sh
+++ updateDefaulter.sh
@@ -4,8 +4,10 @@ for room in $HOME/*; do
     for student in $room/*; do
       if [ -d $student ]; then
         if [ $(awk 'NR==2 {print $2}' $student/fees.txt) -eq 0 ];then
-        rollnumber=$(awk -F ': ' '/Roll Number:/ {print $2}' $student/userDetails.txt)
-        echo $(basename $student) $rollnumber >> $HOME/feeDefaulters.txt
+          if ! grep -q "$(awk -F ': ' '/Roll Number:/ {print $2}' $student/userDetails.txt)" "$HOME/feeDefaulters.txt"; then
+                rollnumber=$(awk -F ': ' '/Roll Number:/ {print $2}' $student/userDetails.txt)
+                echo $(basename $student) $rollnumber >> $HOME/feeDefaulters.txt
+          fi
         fi
       fi
     done
