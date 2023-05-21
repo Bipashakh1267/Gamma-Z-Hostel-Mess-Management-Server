3acc159ff271be71da3e7ded5fbd1bc185e84355
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-05-21 11:57:43 +0530

    Update feeBreakup.sh

--- feeBreakup.sh
+++ feeBreakup.sh
@@ -4,7 +4,7 @@ awk '{ if ($1 == "HostelRent") $2 += 20000; print }' "$HOME/fees.txt" > "$HOME/t
 awk '{ if ($1 == "ServiceCharge") $2 += 10000; print }' "$HOME/fees.txt" > "$HOME/temp.txt" && mv "$HOME/temp.txt" "$HOME/fees.txt"
 awk '{ if ($1 == "MessFee") $2 += 20000; print }' "$HOME/fees.txt" > "$HOME/temp.txt" && mv "$HOME/temp.txt" "$HOME/fees.txt"
 
-echo "Date of fee submission :- $date" >> $HOME/fees.txt
+echo "Date of fee submission :- $(date +%Y-%m-%d)" >> $HOME/fees.txt
 hostel=$(dirname $(dirname $HOME))
 if [ $(cat $hostel/announcements.txt | wc -l) -lt 5 ]
 then
