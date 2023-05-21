4fea4bb2c766f4fb58bd88bf4d266db956495888
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-05-21 11:49:33 +0530

    Update feeBreakup.sh

--- feeBreakup.sh
+++ feeBreakup.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-awk '/TuitionFee/ {$2=$2+50000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
-awk '/HostelRent/ {$2=$2+20000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
-awk '/ServiceCharge/ {$2=$2+10000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
-awk '/MessFee/ {$2=$2+20000; print}' $HOME/fees.txt > $HOME/temp.txt && mv $HOME/temp.txt $HOME/fees.txt
+awk '{ if ($1 == "TuitionFee") $2 += 50000; print }' "$HOME/fees.txt" > "$HOME/temp.txt" && mv "$HOME/temp.txt" "$HOME/fees.txt"
+awk '{ if ($1 == "HostelRent") $2 += 20000; print }' "$HOME/fees.txt" > "$HOME/temp.txt" && mv "$HOME/temp.txt" "$HOME/fees.txt"
+awk '{ if ($1 == "ServiceCharge") $2 += 10000; print }' "$HOME/fees.txt" > "$HOME/temp.txt" && mv "$HOME/temp.txt" "$HOME/fees.txt"
+awk '{ if ($1 == "MessFee") $2 += 20000; print }' "$HOME/fees.txt" > "$HOME/temp.txt" && mv "$HOME/temp.txt" "$HOME/fees.txt"
 
 echo "Date of fee submission :- $date" >> $HOME/fees.txt
 hostel=$(dirname $(dirname $HOME))
