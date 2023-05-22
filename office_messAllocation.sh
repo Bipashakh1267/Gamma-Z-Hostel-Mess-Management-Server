ff134583c038e32b26dd59ca0dc37026dadfd978
Bipashakh1267 <131987861+Bipashakh1267@users.noreply.github.com>
2023-05-23 04:38:26 +0530

    Update office_messAllocation.sh

--- office_messAllocation.sh
+++ office_messAllocation.sh
@@ -11,20 +11,18 @@ tail -n +6 /home/HAD/mess.txt | while read line; do
       if [ -d "$room" ]; then
         for student in "$room"/*; do
          if [ -d "$student" ]; then
-          echo $(basename $student)
           rollnumber=$(grep "Roll Number:" "$student/userDetails.txt" | awk '{print $NF}')
-          echo rollnumber $rollnumber
           if [[ $roll_mess == $rollnumber ]]; then
                messpref=$(grep "Mess Preferences:" $student/userDetails.txt | awk '{print $NF}')
                echo messpref $messpref
                if [[ $messpref == 1 ]] && [[ $mess1 != 0 ]]; then
-                 awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "$student/temp.txt" && mv "$student/temp.txt" "$student/userDetails.txt"  
+                 sudo awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "/home/HAD/temp.txt" && sudo mv "/home/HAD/temp.txt" "$student/userDetails.txt"  
                  ((mess1=mess1-1))
                elif [[ $messpref == 2 ]] && [[ $mess2 != 0 ]]; then
-                 awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "$student/temp.txt" && mv "$student/temp.txt" "$student/userDetails.txt"  
+                 sudo awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "/home/HAD/temp.txt" && sudo mv "/home/HAD/temp.txt" "$student/userDetails.txt"  
                  ((mess2=mess2-1))
                elif [[ $messpref == 3 ]] && [[ $mess3 != 0 ]]; then
-                 awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "$student/temp.txt" && mv "$student/temp.txt" "$student/userDetails.txt"  
+                 sudo awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "/home/HAD/temp.txt" && sudo mv "/home/HAD/temp.txt" "$student/userDetails.txt"  
                  ((mess3=mess3-1))
                fi
           fi
@@ -45,15 +43,15 @@ for hostel in /home/HAD/*; do
           if [[ $(sed -n '8p' "$student/userDetails.txt") == *-* ]]; then
               if [[ $mess1 != 0 ]]; then
                  messpref=1
-                 awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "$student/temp.txt" && mv "$student/temp.txt" "$student/userDetails.txt"  
+                 sudo awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "/home/HAD/temp.txt" && sudo mv "/home/HAD/temp.txt" "$student/userDetails.txt"  
                  ((mess1=mess1-1))
                elif [[ $mess2 != 0 ]]; then
                  messpref=2
-                 awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "$student/temp.txt" && mv "$student/temp.txt" "$student/userDetails.txt"  
+                 sudo awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "/home/HAD/temp.txt" && sudo mv "/home/HAD/temp.txt" "$student/userDetails.txt"  
                  ((mess2=mess2-1))
                elif [[ $mess3 != 0 ]]; then
                  messpref=3
-                 awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "$student/temp.txt" && mv "$student/temp.txt" "$student/userDetails.txt"  
+                 sudo awk -v val="$messpref" '{ if ($1 == "Allocated") $3 = val; print }' "$student/userDetails.txt" > "/home/HAD/temp.txt" && sudo mv "/home/HAD/temp.txt" "$student/userDetails.txt"  
                  ((mess3=mess3-1))
                fi
            fi
